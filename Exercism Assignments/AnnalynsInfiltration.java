bd3942ab849d3914b626dc13c63c8098bf6834fb
Santhosh Swamy V <santhoshswamyv@gmail.com>
2024-03-13 17:36:49 +0530

    Create AnnalynsInfiltration.java

---
+++ Exercism Assignments/AnnalynsInfiltration.java
@@ -0,0 +1,27 @@
+package exercism.annalynsinfiltration;
+
+public class AnnalynsInfiltration {
+
+	public static void main(String[] args) {
+		System.out.println(canFreePrisoner(true, false, false, true));
+
+	}
+
+	public static boolean canFastAttack(boolean knightIsAwake) {
+		return !knightIsAwake;
+	}
+
+	public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+		return (knightIsAwake || archerIsAwake || prisonerIsAwake);
+	}
+
+	public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+		return !archerIsAwake && prisonerIsAwake;
+	}
+
+	public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+			boolean petDogIsPresent) {
+		return petDogIsPresent ? (!archerIsAwake ? true : false)
+				: (!archerIsAwake && !knightIsAwake && prisonerIsAwake) ? true : false;
+	}
+}
