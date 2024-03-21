b7f8ef79e145de691eba0be8018d8bcf6106395d
Santhosh Swamy V <santhoshswamyv@gmail.com>
2024-03-21 22:00:12 +0530

    Update Question15.java

--- Task-2/Question15.java
+++ Task-2/Question15.java
@@ -4,16 +4,14 @@ import java.util.Arrays;
 
 public class Question15 {
 	public static void main(String[] args) {
-		int[] arr = { 1, 2, 3, 4, 4, 4, 5, 5, 6, 7, 8, 9, 9 };
+		int[] arr = { 1, 2, 3, 4, 4, 5, 4, 4, 5, 5, 6, 7, 8, 9, 9 };
 
 		Arrays.sort(arr);
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
-					System.out.println(arr[j]);
-					break;
-				}
+		int prevDuplicate = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] == arr[i + 1] && arr[i] != prevDuplicate) {
+				System.out.println(arr[i]);
+				prevDuplicate = arr[i];
 			}
 		}
 	}
